36a6f615af1d33af0631ad28a8d60219d11810e4
tomwatson6 <tom.watson6@outlook.com>
2022-12-23 18:43:03 +0000

    improved performance of day 23

--- Day23/main.py
+++ Day23/main.py
@@ -3,11 +3,12 @@ from collections import defaultdict
 with open("input.txt") as f:
     input = [list(x.strip()) for x in f.readlines()]
 
-grid = {}
+grid = set()
 
 for r, row in enumerate(input):
-    for c, col in enumerate(row):
-        grid[(r, c)] = col
+    for c, val in enumerate(row):
+        if val == '#':
+            grid.add((r, c))
 
 def get_adjacents(coord):
     adj = []
@@ -23,10 +24,10 @@ def get_adjacents(coord):
     return adj
 
 funcs = {
-    0: lambda k, adj: all([grid.get(x) is None or grid.get(x) == '.' for x in adj if x[0] < k[0]]),
-    1: lambda k, adj: all([grid.get(x) is None or grid.get(x) == '.' for x in adj if x[0] > k[0]]),
-    2: lambda k, adj: all([grid.get(x) is None or grid.get(x) == '.' for x in adj if x[1] < k[1]]),
-    3: lambda k, adj: all([grid.get(x) is None or grid.get(x) == '.' for x in adj if x[1] > k[1]]),
+    0: lambda k, adj: all([x not in grid for x in adj if x[0] < k[0]]),
+    1: lambda k, adj: all([x not in grid for x in adj if x[0] > k[0]]),
+    2: lambda k, adj: all([x not in grid for x in adj if x[1] < k[1]]),
+    3: lambda k, adj: all([x not in grid for x in adj if x[1] > k[1]]),
 }
 
 outcomes = {
@@ -38,44 +39,31 @@ outcomes = {
 
 func_pointer = 0
 
-def evolve(rounds):
+def evolve():
     global func_pointer
     M = defaultdict(lambda: [])
-    for k, v in grid.items():
-        if v == '#':
-            adj = get_adjacents(k)
-            if any([grid.get(x) is not None and grid.get(x) == '#' for x in adj]):
-                if funcs[func_pointer % len(funcs)](k, adj):
-                    M[outcomes[func_pointer % len(funcs)](k)].append(k)
-                elif funcs[(func_pointer + 1) % len(funcs)](k, adj):
-                    M[outcomes[(func_pointer + 1) % len(funcs)](k)].append(k)
-                elif funcs[(func_pointer + 2) % len(funcs)](k, adj):
-                    M[outcomes[(func_pointer + 2) % len(funcs)](k)].append(k)
-                elif funcs[(func_pointer + 3) % len(funcs)](k, adj):
-                    M[outcomes[(func_pointer + 3) % len(funcs)](k)].append(k)
+    for k in grid:
+        adj = get_adjacents(k)
+        if any([x in grid for x in adj]):
+            if funcs[func_pointer % len(funcs)](k, adj):
+                M[outcomes[func_pointer % len(funcs)](k)].append(k)
+            elif funcs[(func_pointer + 1) % len(funcs)](k, adj):
+                M[outcomes[(func_pointer + 1) % len(funcs)](k)].append(k)
+            elif funcs[(func_pointer + 2) % len(funcs)](k, adj):
+                M[outcomes[(func_pointer + 2) % len(funcs)](k)].append(k)
+            elif funcs[(func_pointer + 3) % len(funcs)](k, adj):
+                M[outcomes[(func_pointer + 3) % len(funcs)](k)].append(k)
 
-    new_grid = {}
-    moved = set()
+    new_grid = set()
 
     for k, v in M.items():
         if len(v) > 1:
             continue
 
-        new_grid[k] = grid[v[0]]
-        moved.add(v[0])
+        new_grid.add(k)
+        grid.remove(v[0])
 
-    RS = max([x[0] for x, y in grid.items() if y == '#'])
-    CS = max([x[1] for x, y in grid.items() if y == '#'])
-
-    for r in range(-rounds, RS + 1 + rounds):
-        for c in range(-rounds, CS + 1 + rounds):
-            if (r, c) in moved:
-                new_grid[(r, c)] = '.'
-            elif new_grid.get((r, c)) is None:
-                if grid.get((r, c)) is not None:
-                    new_grid[(r, c)] = grid[(r, c)]
-                else:
-                    new_grid[(r, c)] = '.'
+    new_grid = new_grid.union(grid)
 
     func_pointer += 1
     
@@ -84,60 +72,60 @@ def evolve(rounds):
 def count_empty():
     count = 0
 
-    YM = min([x[0] for x, y in grid.items() if y == '#'])
-    XM = min([x[1] for x, y in grid.items() if y == '#'])
-    YS = max([x[0] for x, y in grid.items() if y == '#'])
-    XS = max([x[1] for x, y in grid.items() if y == '#'])
+    YM = min([x[0] for x in grid])
+    XM = min([x[1] for x in grid])
+    YS = max([x[0] for x in grid])
+    XS = max([x[1] for x in grid])
 
     for r in range(YM, YS + 1):
         for c in range(XM, XS + 1):
-            if grid.get((r, c)) is not None and grid.get((r, c)) == '.':
+            if (r, c) in grid:
                 count += 1
 
-    return count
+    return (YS - YM + 1) * (XS - XM + 1) - count
 
 def print_grid():
-    YM = min([x[0] for x, y in grid.items() if y == '#'])
-    XM = min([x[1] for x, y in grid.items() if y == '#'])
-    YS = max([x[0] for x, y in grid.items() if y == '#'])
-    XS = max([x[1] for x, y in grid.items() if y == '#'])
+    YM = min([x[0] for x in grid])
+    XM = min([x[1] for x in grid])
+    YS = max([x[0] for x in grid])
+    XS = max([x[1] for x in grid])
 
     for r in range(YM, YS + 1):
         for c in range(XM, XS + 1):
-            if grid.get((r, c)) is not None:
-                print(grid[(r, c)], end="")
+            if (r, c) in grid:
+                print('#', end="")
             else:
-                print("X", end = "")
+                print("X", end="")
         print()
 
     print()
 
 def trim_grid():
-    YM = min([x[0] for x, y in grid.items() if y == '#'])
-    XM = min([x[1] for x, y in grid.items() if y == '#'])
-    YS = max([x[0] for x, y in grid.items() if y == '#'])
-    XS = max([x[1] for x, y in grid.items() if y == '#'])
+    YM = min([x[0] for x in grid])
+    XM = min([x[1] for x in grid])
+    YS = max([x[0] for x in grid])
+    XS = max([x[1] for x in grid])
 
-    new_grid = {}
+    new_grid = set()
 
     for r in range(YM, YS + 1):
         for c in range(XM, XS + 1):
-            if grid.get((r, c)) is not None:
-                new_grid[(r, c)] = grid[(r, c)]
+            if (r, c) in grid:
+                new_grid.add((r, c))
 
     return new_grid
 
-# print_grid()
-
-for i in range(1000):
-    grid, same = evolve(i)
+same = False
+i = 0
+while not same:
+    grid, same = evolve()
     if same:
-        print(i + 1)
+        print("Part 2:", i + 1)
         break
     grid = trim_grid()
-    print(i, len(grid))
     if i == 9:
-        print(count_empty())
+        print("Part 1:", count_empty())
+    i += 1
 
 
 
